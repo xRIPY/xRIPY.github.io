70bdccfcde538a499d0355d9261ea7c45e50d593
xRIPY <31014875+xRIPY@users.noreply.github.com>
2017-12-12 23:11:39 +0100

    g
    g

--- js/3Dobj.js
+++ js/3Dobj.js
@@ -1,6 +1,14 @@
 $(function(){
 	$('html').on("click",function(){
-		$('#_canvas').fadeToggle(200);
+		$('#_canvas').fadeToggle(400);
+	});
+	
+	$("img:not(:first)").hide();
+	$('.slide').width(window.innerWidth - 50);
+
+	$('.slide').on("click",function(){
+		$(this).next('img').slideToggle(200);
+		$('#_canvas').fadeToggle(0);
 	});
 	
 	//global stuff-----------------------------------------------------------------------------
